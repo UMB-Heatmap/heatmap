1d65d7fc81be07cae53e2a5b869d2ab985b2b5d5
sradel17 <159499155+sradel17@users.noreply.github.com>
2024-04-08 19:33:44 -0400

    Delete LCG.cpp
    Uploaded new lcg.h to 'include' directory

--- LCG.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-#include <cmath>
-#include "LCG.h"
-
-uint64_t lcg(uint64_t seed) {
-    uint64_t mod = pow(2, 31), mult = 1103515245, inc = 12345;
-    return ((mult * seed) + inc) % mod;
-}
