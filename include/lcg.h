df0305346c16e48907fe3a343aeddad56bcb1947
sradel17 <159499155+sradel17@users.noreply.github.com>
2024-04-08 19:28:32 -0400

    Linear congruential generator (LCG) algorithm

---
+++ include/lcg.h
@@ -0,0 +1,19 @@
+//Linear Congruential Generator (LCG) algorithm
+#ifndef A_LCG
+#define A_LCG
+
+#include "algorithm.h"
+
+class LCG : public Algorithm {
+public:
+    LCG(uint64_t seed) : Algorithm(seed) {};
+    uint64_t peekNext() {
+        uint64_t x = this->state;
+        uint64_t mod = 2147483648, mult = 1103515245, inc = 12345;
+        x = ((mult * x) + inc) % mod;
+        
+        return x;
+    };
+};
+
+#endif
