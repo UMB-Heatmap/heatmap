7c2821cc65bd3a2790984745eb415aef1a7b5380
cthomas2310 <mail@christhom.com>
2024-04-16 17:51:33 -0400

    update seed inc

--- src/2d.py
+++ src/2d.py
@@ -19,7 +19,7 @@ import visuals_utils as vis
 # main.py validates user input so we can assume proper CLI input
 ALGORITHM = sys.argv[1]
 START_SEED = int(sys.argv[2])
-SEED_INCREMENT = 1 # default value
+SEED_INCREMENT = 12345 # default value
             
 # STEP 1: Acquire and Validate visualization-specific inputs
 numRows = vis.getIntFromInput("Number of Rows: ")
