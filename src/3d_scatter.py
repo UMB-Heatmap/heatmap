f849a62b2ec740923f6e188fad16b0c03346903c
cthomas2310 <mail@christhom.com>
2024-04-16 17:49:22 -0400

    3d_scatter visual
    - interactive 3d scatter plot of random points
    - documentation cleanup

--- src/3d_scatter.py
+++ src/3d_scatter.py
@@ -1,5 +1,18 @@
-# WIP
-# 3d scatter plot of randomly selected 3d points
+# Interactive 3D scatter plot of randomly generated 3D points
+#
+# Visuals are called from main.py with initial parameters algorithm and seed:
+#   python3 3d_scatter.py (algorithm) (seed)
+#
+# additional parameters (from standard input):
+#   maximum number of points
+#   color map
+#   color mode
+#
+# sliders (for interactive 3D plot):
+#    number of points shown
+#    size of points
+# 
+# can use native matplotlib zoom to explore distribution of random points
 
 from pylab import *
 
@@ -9,64 +22,113 @@ import matplotlib.pylab as plt
 import sys
 import visuals_utils as vis
 
-
 # main.py validates user input so we can assume proper CLI input
 ALGORITHM = sys.argv[1]
 START_SEED = int(sys.argv[2])
-SEED_INCREMENT = 1 # default value
-            
+SEED_INCREMENT = 12345 # default value
+COLOR_MODES_NAMES = ['random', 'diagonal gradient', 'x gradient', 'y gradient', 'z gradient']
+COLOR_MODES = range(1, len(COLOR_MODES_NAMES) + 1)
+N = 100 # default scalar for random values
+
+# display color mode options
+def printColorModeOptions():
+    print("\nColor Modes -- Select From:")
+    for i in COLOR_MODES:
+        print("\t" + str(i) + ": " + COLOR_MODES_NAMES[i-1])
+    print()
+
 # STEP 1: Acquire and Validate visualization-specific inputs
-N = vis.getIntFromInput("NxNxN Heatmap... Specify N: ")
-numVals = vis.getIntFromInput("Number of Random Points: ")
+MAX_VALUES = vis.getIntFromInput("Maximum Number of Random Points: ")
 colorMap = vis.getColorMap()
+printColorModeOptions()
+colorMode = vis.getIntFromInput("Color Mode: ")
+
+while (colorMode not in COLOR_MODES):
+    colorMode = vis.getIntFromInput("Invalid Input -- Select From:\n\t1: random\n\t2: diagonal gradient\n\t3: x gradient\n\t4: y gradient\n\t5: z gradient\n\nColor Mode: ")
 
 # STEP 2: Generate data for visualization via prng.cpp calls
-#   to get NUM_VALUES random doubles in range [0, 1) using ALGORITHM and SEED:
-#
-#       ./prng -f data/OUTPUT_FILENAME.txt -a ALGORITHM -s SEED -n NUM_VALUES
-#   
-#   then read random numbers from txt file data/OUTPUT_FILENAME.txt 
-#
-#   NOTE: output txt files are meant as intermediate data storage so naming is arbitrary
 data = []
-for i in range(3):
-    for n in range(N):
-        filePath = "data/output.txt"
-        cmd = './prng -f ' + filePath + ' -a ' + str(ALGORITHM) + ' -s ' + str(START_SEED + n * SEED_INCREMENT) + ' -n ' + str(numVals)
-        run(cmd, shell=True)
-        nums = np.genfromtxt(filePath)
-        axis = []
-        if (nums.size == 1):
-            axis = [nums.item()]
-        else:
-            axis = list(nums)
-        
-        # scale values
-        for i in range(len(axis)):
-            axis[i] = axis[i] * N
-        
-        data.append(axis)
+if (colorMode == 1): numAxis = 4 
+else: numAxis = 3
+for i in range(numAxis):
+    filePath = "data/output.txt"
+    cmd = './prng -f ' + filePath + ' -a ' + str(ALGORITHM) + ' -s ' + str(START_SEED + i * SEED_INCREMENT) + ' -n ' + str(MAX_VALUES)
+    run(cmd, shell=True)
+    nums = np.genfromtxt(filePath)
+    if (nums.size == 1): axis = [nums.item()]
+    else: axis = list(nums)
+    for i in range(MAX_VALUES): # scale values
+        axis[i] = axis[i] * N
+    data.append(axis) # append axis of numVals values scaled to [0, N)
     
-
 # STEP 3: Generate visualization
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-ax.scatter(data[0], data[1], data[2])
 
-color_map = cm.ScalarMappable(cmap=cm.Greens_r)
-color_map.set_array([data[0] + data[1] + data[2]])
+# Horizontal slider to control number of points displayed
+axNumPoints = fig.add_axes([0.25, 0.1, 0.65, 0.03])
+numPoints_slider = Slider(
+    ax=axNumPoints,
+    label='Random Points',
+    valmin=0,
+    valmax=MAX_VALUES,
+    valinit=round(MAX_VALUES / 10),
+)
+# Vertical slider to control size of random points
+axPointSize = fig.add_axes([0.1, 0.25, 0.0225, 0.63])
+pointSize_slider = Slider(
+    ax=axPointSize,
+    label='Point Size',
+    valmin=0.1,
+    valmax=20,
+    valinit=5,
+    orientation='vertical'
+)
+# update plot when slider value changes
+def update(val):
+    values = round(numPoints_slider.val)
+    ax.cla()
+    ax.scatter(
+        x[0:values], 
+        y[0:values], 
+        z[0:values], 
+        s=pointSize_slider.val, 
+        c=color[0:values], 
+        marker='o', 
+        cmap=colorMap, 
+        alpha=1
+    )
+# register update function with NumPoints slider
+numPoints_slider.on_changed(update)
+pointSize_slider.on_changed(update)
 
-ax.scatter(data[0], data[1], data[2])
+# unpack data / colors
+x = data[0]
+y = data[1]
+z = data[2]
+if (colorMode == 1): # random
+    color = data[3] 
+elif (colorMode == 2): # diagonal gradient
+    color = [0] * MAX_VALUES
+    for i in range(MAX_VALUES):
+        color[i] = (x[i] + y[i] + z[i])
+elif (colorMode == 3):  # x gradient
+    color = x.copy()
+elif (colorMode == 4): # y gradient
+    color = y.copy()
+elif (colorMode == 5): # z gradient
+    color = z.copy()
 
-ax.set_xlabel('X-axis')
-ax.set_ylabel('Y-axis')
-ax.set_zlabel('Z-axis')
-plt.title(str(N) + "x" + str(N) + "x" + str(N) + " Heat Map from " + ALGORITHM.upper())
-
-# STEP 4: Save visualization in heatmaps folder with appropriate name
-heatmapPath = 'heatmaps/' + str(ALGORITHM) + '_' + str(N) + 'x' + str(N) + 'x' + str(N) + '_3d_heatmap.svg'
-# plt.savefig(heatmapPath)
+# initialize plot
+values = round(numPoints_slider.val)
+ax.scatter(
+    x[0:values], 
+    y[0:values], 
+    z[0:values], 
+    c=color[0:values], 
+    marker='o', 
+    cmap=colorMap, 
+    alpha=1
+)
 plt.show()
 
-# STEP 5: Open visualization 
-# cmd = 'open ' + heatmapPath
-# run(cmd, shell=True)
+
