ff7b12b080b39506ddc6f082a32cc9802f5d7be3
[ LEEJER ] <leeijer@gmail.com>
2024-04-23 13:57:02 -0400

    four py

--- src/visuals_utils.py
+++ src/visuals_utils.py
@@ -6,7 +6,7 @@ DEFAULT_SEED = 1
 DEFAULT_SEED_INCREMENT = 12345
 
 # add algorithm options HERE: (must be all lowercase)
-ALGORITHMS = ['lehmer', 'splitmix', 'xorshift', 'lcg', 'middle_square', 'rule30', 'lfg']
+ALGORITHMS = ['lehmer', 'splitmix', 'xorshift', 'lcg', 'middle_square', 'rule30', 'lfg', 'four']
 HAS_EXTRA_ARGS = ['lfg']
 
 # add visual options HERE: (must be all lowercase and same as python script name)
